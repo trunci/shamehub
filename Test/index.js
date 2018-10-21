7ae930e15de7c8da2ca4bfb1d8da0637c4bb9ec8
trunci <eduardotrunci@gmail.com>
2018-10-21 02:00:07 -0400

    Inserted additional functionalities, including the proper Facebook's API starting

--- Test/index.js
+++ Test/index.js
@@ -1,25 +1,3 @@
-window.fbAsyncInit = function() {
-    FB.init({
-        appId            : '299447580655569',
-        autoLogAppEvents : true,
-        xfbml            : true,
-        version          : 'v3.1'
-    });
-};
-(function(d, s, id){
-    var js, fjs = d.getElementsByTagName(s)[0];
-    if (d.getElementById(id)) {return;}
-    js = d.createElement(s); js.id = id;
-    js.src = "https://connect.facebook.net/en_US/sdk.js";
-    fjs.parentNode.insertBefore(js, fjs);
-}(document, 'script', 'facebook-jssdk'));
-
-
-FB.getLoginStatus(function(response) {
-    statusChangeCallback(response);
-});
-
-
 function login() {
     FB.login(function(response) {
         console.log(response);
