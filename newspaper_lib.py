b1283eaea3c41d6f08181c6d1429def28430171a
Davi Nakajima An <dan36@gatech.edu>
2018-10-21 04:34:48 -0400

    added keywords builder and json file

--- newspaper_lib.py
+++ newspaper_lib.py
@@ -1,11 +1,30 @@
+import json
 import newspaper
 from newspaper import Article
+import imp
 
-op_eds = newspaper.build('https://www.cnn.com')
+controversial_urls = ['https://www.cnn.com/2018/01/10/opinions/the-problem-isnt-trumps-brain-but-his-heart-opinion-dantonio/index.html', 
+        'https://www.cnn.com/2018/01/09/opinions/donald-trump-college-football-national-anthem-filipovic-opinion/index.html',
+        'https://www.cnn.com/2018/01/09/opinions/tps-salvadoran-immigration-opinion-reyes/index.html',
+        'https://www.cnn.com/2018/01/03/opinions/how-trump-can-avoid-north-korea-catastrophetim-kaine/index.html']
 
-i = 0
-print(len(op_eds.articles))
-for art in op_eds.articles:
-    print (art.url)
-    i += 1
+articles = []
+keywords = []
+
+for url in controversial_urls:
+    articles.append(Article(url))
+    #print(url)
+
+for article in articles:
+    article.download()
+    article.parse()
+    article.nlp()
+    for word in article.keywords:
+        keywords.append(word)
+print (keywords)
+
+dict_entry = {'controversial_words' : keywords}
+
+with open('keywords.json', 'w') as outfile:
+    json.dump(dict_entry, outfile)
 
