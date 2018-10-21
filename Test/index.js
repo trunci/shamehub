1cf411a4d3674af3b4116d3dfb6042393c25b1ca
trunci <eduardotrunci@gmail.com>
2018-10-21 02:07:20 -0400

    Changed the palces of the JS inside the HTML

--- Test/index.js
+++ Test/index.js
@@ -1,3 +1,24 @@
+ window.fbAsyncInit = function() {
+    FB.init({
+      appId      : '299447580655569',
+      cookie     : true,
+      xfbml      : true,
+      version    : 'v3.1'
+    });
+      
+    FB.AppEvents.logPageView();   
+      
+  };
+
+  (function(d, s, id){
+     var js, fjs = d.getElementsByTagName(s)[0];
+     if (d.getElementById(id)) {return;}
+     js = d.createElement(s); js.id = id;
+     js.src = "https://connect.facebook.net/en_US/sdk.js";
+     fjs.parentNode.insertBefore(js, fjs);
+   }(document, 'script', 'facebook-jssdk'));
+
+
 function login() {
     FB.login(function(response) {
         console.log(response);
