88203ad69b3994462130030933c867f12c4302bd
trunci <eduardotrunci@gmail.com>
2018-12-01 00:29:43 -0500

    Database added. Files upadte in firebase>shamehub

--- firebase/shamehub/index.js
+++ firebase/shamehub/index.js
@@ -1,6 +1,8 @@
 // Initialize the Facebook API, using the App's unique Id
 // Basically interval given FB stuff
 
+
+
 window.fbAsyncInit = function() {
 	FB.init({
 	  appId      : '299447580655569',
@@ -11,6 +13,7 @@ window.fbAsyncInit = function() {
 	FB.AppEvents.logPageView();   
 };
 
+
 (function(d, s, id){
 	var js, fjs = d.getElementsByTagName(s)[0];
 	if (d.getElementById(id)) {return;}
@@ -18,6 +21,81 @@ window.fbAsyncInit = function() {
 	js.src = "https://connect.facebook.net/en_US/sdk.js";
 	fjs.parentNode.insertBefore(js, fjs);
 }(document, 'script', 'facebook-jssdk'));
+
+
+
+
+// Initialize Firebase Database
+
+var config = {
+	apiKey: "AIzaSyBr4MdONYacDGIdh5z-Xm3Qr9gBDRLFmkc",
+	authDomain: "shamehub-101010.firebaseapp.com",
+	databaseURL: "https://shamehub-101010.firebaseio.com",
+	projectId: "shamehub-101010",
+	storageBucket: "shamehub-101010.appspot.com",
+	messagingSenderId: "955219006464",
+};
+
+firebase.initializeApp(config);
+var database = firebase.database();
+
+
+// Check to see the info on the user or create a new user
+function checkUser(name, userId){
+	// Check if user has already used the website
+	let a;
+	database.ref('users/' + userId).on('value', function(snapshot) {
+		a = snapshot; 
+	});
+
+	if (a == undefined) {
+		// Create new User
+		createNewUser(name, userId);
+		paid = false;
+	} else {
+
+		if (a.paid == "no"){
+			paid = false;
+		} else {
+			paid = true;
+		}
+
+	}
+}
+
+
+// Function to create a new user
+function createNewUser(name, userId){
+	  firebase.database().ref('users/' + userId).set({
+	    username: name,
+	    paid: "no"
+  });
+}
+
+
+// Declare this function to make statusChangeCallBack work
+
+function statusChangeCallback(response) {
+            console.log('statusChangeCallback');
+            console.log(response);
+            // The response object is returned with a status field that lets the
+            // app know the current login status of the person.
+            // Full docs on the response object can be found in the documentation
+            // for FB.getLoginStatus().
+            if (response.status === 'connected') {
+                // Logged into your app and Facebook.
+                console.log('Welcome!  Fetching your information.... ');
+                FB.api('/me', function (response) {
+                    console.log('Successful login for: ' + response.name);
+                    document.getElementById('status').innerHTML =
+                      'Thanks for logging in, ' + response.name + '!';
+                });
+            } else {
+                // The person is not logged into your app or we are unable to tell.
+                document.getElementById('status').innerHTML = 'Please log ' +
+                  'into this app.';
+            }
+        }
 
 
 // Function to see if the user has logged in. Is called after the person's login has been completed
@@ -41,6 +119,12 @@ function startWithName() {
 		welcome.id = "welcome";
 		welcome.class = "welcome";
 		welcome.innerHTML = "Hi, " + response.name + "!"
+		console.log(response.id);
+		if (response.name != undefined) {
+			checkUser(response.name, response.id);
+		} else {
+			location.reload();
+		}
 		document.getElementById("perfil").appendChild(welcome);
 	});
 	// Calls the next function
@@ -110,14 +194,14 @@ function generateBoxes(response) {
 		message = response.data[i].message;
 		id = response.data[i].id;
 		if (message != null){
-			message1 = message.toLowerCase();
+			message = message.toLowerCase();
 		}
 
 		for (x=0; x<700; x++){
 			console.log(message);
 			console.log(x);
 			console.log(list);
-			if (message1 != null && message1.search(list[x]) != -1 && document.getElementById(id) == null){
+			if (message != null && message.search(list[x]) != -1 && document.getElementById(id) == null){
 				generateBox(id, time, message);
 				found = found + 1;
 			}
